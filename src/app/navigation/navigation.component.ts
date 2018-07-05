e48b5021116e380970c63d5df267d9546d20c875
NivDolev <dolevniv@gmail.com>
2018-07-05 11:32:52 +0300

    navigation subscribed to login

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LogingService } from './../services/login.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit , OnDestroy{
 
-  constructor() { }
+  activcatedSubscription: Subscription;
+  private _isLogged: boolean;
+  get isLogged(): boolean {
+    return this._isLogged;
+  }
+  constructor(private logingService: LogingService) { }
 
   ngOnInit() {
+    this.registerLogin();
   }
 
+  ngOnDestroy() {
+    this.activcatedSubscription.unsubscribe();
+  }
+
+  registerLogin(): void {
+    this.activcatedSubscription = this.logingService.getLoginStatus()
+    .subscribe(
+      (logged: boolean) => {
+        this._isLogged = logged;
+      }
+    );
+  }
 }
